3d7fe5a94eddb5ea3f65c536776b09f28c5ac51b
Byron Sommardahl <byron@sommardahl.com>
2017-05-29 13:22:00 +0000

    Removing unused code.

--- src/ICommandDispatcher.ts
+++ src/ICommandDispatcher.ts
@@ -1,7 +1,6 @@
-import { IEvent } from './IEvent';
 import { ICommand } from './ICommand';
 import { Observable } from 'rxjs/Observable';
 
 export interface ICommandDispatcher {
-    dispatch(command: ICommand): Observable<IEvent>;
+    dispatch(command: ICommand): Observable<any>;
 }
--- src/dispatchers/ImmediateCommandDispatcher.ts
+++ src/dispatchers/ImmediateCommandDispatcher.ts
@@ -1,5 +1,4 @@
 import { ICommand } from '../ICommand';
-import { IEvent } from '../IEvent';
 import { ICommandDispatcher } from '../ICommandDispatcher';
 import { Observable } from 'rxjs/Observable';
 import * as Rx from 'rxjs';
@@ -9,7 +8,7 @@ export class ImmediateCommandDispatcher implements ICommandDispatcher {
   constructor(private commandHandlers: any[]) {
   }
 
-  dispatch(command: ICommand): Observable<IEvent> {
+  dispatch(command: ICommand): Observable<any> {
     return Rx.Observable.from(this.commandHandlers
         .filter((h) => h[command.type])
         .map((h) => h[command.type](command)));
