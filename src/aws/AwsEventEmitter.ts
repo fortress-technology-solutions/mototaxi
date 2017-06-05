094db48bb8495fcd548d64ab3771eafc213631ce
Byron Sommardahl <byron@acklenavenue.com>
2017-06-05 14:14:20 -0600

    Added hack to make the compiler accept a module. :

--- src/aws/AwsEventEmitter.ts
+++ src/aws/AwsEventEmitter.ts
@@ -1,11 +1,14 @@
+'use strict';
+
 import { IEventEmitter } from '../IEventEmitter';
 import { ISqsConfig } from './ISqsConfig';
-import * as sqs from 'sqs';
+import * as _sqs from 'sqs';
 
 export class AwsEventEmitter implements IEventEmitter {
     private queue: any;
 
     constructor(private config: ISqsConfig, private log?: (msg) => void) {
+        const sqs =  _sqs;
         this.queue = sqs(config);
     }
 
