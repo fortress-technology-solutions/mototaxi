2a3574a86788123d75d1b396c48bb99cf9adca60
Byron Sommardahl <byron@acklenavenue.com>
2017-06-06 10:50:44 -0600

    Improving comamnd dispatcher.

--- src/CommandDispatcher.ts
+++ src/CommandDispatcher.ts
@@ -3,7 +3,6 @@ import { ICommandDispatcher } from './ICommandDispatcher';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import { IEventEmitter } from './IEventEmitter';
-import * as Rx from 'rxjs';
 
 export class CommandDispatcher implements ICommandDispatcher {
 
@@ -11,23 +10,14 @@ export class CommandDispatcher implements ICommandDispatcher {
 
     constructor(private commandHandlers: any[], private eventEmitter: IEventEmitter) {
         this.stream = new Subject();
-
-        Rx.Observable.from(this.commandHandlers)
-            .map((handler) => {
-                return { keys: Object.keys(handler), handler };
-            })
-            .flatMap(({keys, handler}) => keys.map((commandType) => {
-                    return { commandType, handler};
-            }))
-            .subscribe(({commandType, handler}) => {
-                this.eventEmitter.on(commandType, (command) => {
-                    const domainEvent = handler[commandType](command);
-                    this.stream.next(domainEvent);
-                });
-            });
     }
 
     dispatch(command: ICommand): Observable<any> {
+        this.eventEmitter.on(command.type, (cmd) => {
+            this.commandHandlers.filter((handler) => handler[cmd.type])
+                .map((handler) => handler[cmd.type](cmd))
+                .forEach((domainEvent) => this.stream.next(domainEvent));
+        });
         setTimeout(() => this.eventEmitter.emit(command.type, command), 0);
         return this.stream;
     }
