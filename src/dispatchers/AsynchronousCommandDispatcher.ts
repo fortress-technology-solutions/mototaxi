32946033cdb3e4218938f65c081d11fed3d585db
Byron Sommardahl <byron@acklenavenue.com>
2017-06-08 15:56:45 -0600

    Putting the stupit inferior settimeout back.

--- src/dispatchers/AsynchronousCommandDispatcher.ts
+++ src/dispatchers/AsynchronousCommandDispatcher.ts
@@ -14,17 +14,17 @@ export class AsynchronousCommandDispatcher implements ICommandDispatcher {
     dispatch(command: ICommand): Observable<any> {
         const receiptId = `${command.type}-${new Date().getTime()}`;
 
-        Rx.Observable.interval(20)
-            .throttle(val => Rx.Observable.interval(20))
-            .first()
-            .subscribe(() => {
-                this.eventEmitter.emit(receiptId, command);
-            });
+        // Rx.Observable.interval(20)
+        //     .throttle(val => Rx.Observable.interval(20))
+        //     .first()
+        //     .subscribe(() => {
+        //         this.eventEmitter.emit(receiptId, command);
+        //     });
 
-        // setTimeout(() => {
-        //     this.log(`CommandDispatcher: Emitting command: ${receiptId}`);
-        //     this.eventEmitter.emit(receiptId, command);
-        // }, 50);
+        setTimeout(() => {
+            this.log(`CommandDispatcher: Emitting command: ${receiptId}`);
+            this.eventEmitter.emit(receiptId, command);
+        }, 50);
 
         this.log(`CommandDispatcher: Listening for ${receiptId}...`);
         return Rx.Observable.fromEvent((this.eventEmitter as any), receiptId);
